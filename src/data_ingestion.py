5cf8e3967a95fc0e90c72d6cc427b01136259ff2
GerardCB <gerardcalvo26@gmail.com>
2023-07-27 15:34:01 +0200

    Pre-made XML parsing script

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -1,39 +1,79 @@
 import argparse
 import datetime
-import requests
 import pandas as pd
-from bs4 import BeautifulSoup
+from utils import perform_get_request, xml_to_load_dataframe, xml_to_gen_data
 
-def get_data_from_entsoe(start_time, end_time):
+def get_load_data_from_entsoe(regions, periodStart='202302240000', periodEnd='202303240000', output_path='./data'):
+    
+    # TODO: There is a period range limit of 1 year for this API. Process in 1 year chunks if needed
+    
     # URL of the RESTful API
-    url = "https://transparency.entsoe.eu/restfulAPI/"
+    url = 'https://web-api.tp.entsoe.eu/api'
 
-    # TODO: Use the requests library to get data from the API for the specified time range
-    # TODO: Parse the data into a pandas DataFrame
+    # General parameters for the API
+    # Refer to https://transparency.entsoe.eu/content/static_content/Static%20content/web%20api/Guide.html#_documenttype
+    params = {
+        'securityToken': '1d9cd4bd-f8aa-476c-8cc1-3442dc91506d',
+        'documentType': 'A65',
+        'processType': 'A16',
+        'outBiddingZone_Domain': 'FILL_IN', # used for Load data
+        'periodStart': periodStart, # in the format YYYYMMDDHHMM
+        'periodEnd': periodEnd # in the format YYYYMMDDHHMM
+    }
 
-    return df_entsoe
+    # Loop through the regions and get data for each region
+    for region, area_code in regions.items():
+        print(f'Fetching data for {region}...')
+        params['outBiddingZone_Domain'] = area_code
+    
+        # Use the requests library to get data from the API for the specified time range
+        response_content = perform_get_request(url, params)
 
-def get_data_from_csv():
-    # URL of the CSV file
-    url = "https://example.com/data.csv"
+        # Response content is a string of XML data
+        df = xml_to_load_dataframe(response_content, 'Load')
 
-    # TODO: Use pandas read_csv function to get data from the CSV file
+        # Save the DataFrame to a CSV file
+        df.to_csv(f'{output_path}/load_{region}.csv', index=False)
+       
+    return
 
-    return df_csv
+def get_gen_data_from_entsoe(regions, periodStart='202302240000', periodEnd='202303240000', output_path='./data'):
+    
+    # TODO: There is a period range limit of 1 day for this API. Process in 1 day chunks if needed
 
-def get_data_from_website():
-    # URL of the website
-    url = "https://example.com"
+    # URL of the RESTful API
+    url = 'https://web-api.tp.entsoe.eu/api'
 
-    # TODO: Use the requests library to get the HTML of the page
-    # TODO: Use BeautifulSoup to parse the HTML
-    # TODO: Extract the data and parse it into a pandas DataFrame
+    # General parameters for the API
+    params = {
+        'securityToken': '1d9cd4bd-f8aa-476c-8cc1-3442dc91506d',
+        'documentType': 'A75',
+        'processType': 'A16',
+        'outBiddingZone_Domain': 'FILL_IN', # used for Load data
+        'in_Domain': 'FILL_IN', # used for Generation data
+        'periodStart': periodStart, # in the format YYYYMMDDHHMM
+        'periodEnd': periodEnd # in the format YYYYMMDDHHMM
+    }
 
-    return df_website
+    # Loop through the regions and get data for each region
+    for region, area_code in regions.items():
+        print(f'Fetching data for {region}...')
+        params['outBiddingZone_Domain'] = area_code
+        params['in_Domain'] = area_code
+    
+        # Use the requests library to get data from the API for the specified time range
+        response_content = perform_get_request(url, params)
 
-def save_data(df, output_file):
-    # TODO: Save the DataFrame to a CSV file
-    pass
+        # Response content is a string of XML data
+        dfs = xml_to_gen_data(response_content)
+
+        # Save the dfs to CSV files
+        for psr_type, df in dfs.items():
+            # Save the DataFrame to a CSV file
+            df.to_csv(f'{output_path}/gen_{region}_{psr_type}.csv', index=False)
+    
+    return
+
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Data ingestion script for Energy Forecasting Hackathon')
@@ -46,26 +86,41 @@ def parse_arguments():
     parser.add_argument(
         '--end_time', 
         type=lambda s: datetime.datetime.strptime(s, '%Y-%m-%d'), 
-        default=datetime.datetime(2023, 7, 24), 
+        default=datetime.datetime(2023, 1, 2), 
         help='End time for the data to download, format: YYYY-MM-DD'
     )
     parser.add_argument(
-        '--output_file', 
+        '--output_path', 
         type=str, 
-        default='data.csv', 
+        default='./data',
         help='Name of the output file'
     )
     return parser.parse_args()
 
-def main(start_time, end_time, output_file):
-    df_entsoe = get_data_from_entsoe(start_time, end_time)
-    df_csv = get_data_from_csv()
-    df_website = get_data_from_website()
-
-    # TODO: Merge the data into a single DataFrame
+def main(start_time, end_time, output_path):
     
-    save_data(df, output_file)
+    regions = {
+        'HU': '10YHU-MAVIR----U',
+        'IT': '10YIT-GRTN-----B',
+        'PO': '10YPL-AREA-----S',
+        'SP': '10YES-REE------0',
+        'UK': '10Y1001A1001A92E',
+        'DE': '10Y1001A1001A83F',
+        'DK': '10Y1001A1001A65H',
+        'SE': '10YSE-1--------K',
+        'NE': '10YNL----------L',
+    }
+
+    # Transform start_time and end_time to the format required by the API: YYYYMMDDHHMM
+    start_time = start_time.strftime('%Y%m%d%H%M')
+    end_time = end_time.strftime('%Y%m%d%H%M')
+
+    # Get Load data from ENTSO-E
+    get_load_data_from_entsoe(regions, start_time, end_time, output_path)
+
+    # Get Generation data from ENTSO-E
+    get_gen_data_from_entsoe(regions, start_time, end_time, output_path)
 
 if __name__ == "__main__":
     args = parse_arguments()
-    main(args.start_time, args.end_time, args.output_file)
+    main(args.start_time, args.end_time, args.output_path)
